5d8ddb7683b3913c8e961332be2604cd5803db43
omeryentur <63314411+omeryentur@users.noreply.github.com>
2020-09-07 02:47:00 +0300

    Update LogisticRegression.py

--- Classification/Logistick Regression/LogisticRegression.py
+++ Classification/Logistick Regression/LogisticRegression.py
@@ -2,12 +2,12 @@ import pandas as  pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
+import numpy as np
 #veriyi hazırlama
 pf=pd.read_csv("../../Datasets/Cancer.csv")
 X=pf.drop(['Unnamed: 32',"id","diagnosis"],axis=1)
-Y=pd.get_dummies(pf['diagnosis'], drop_first=True)
-
+Y=np.array(pd.get_dummies(pf['diagnosis'], drop_first=True)).reshape(X.shape[0])
+print((Y.shape))
 #veriyi bölme
 X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.21,random_state=42)
 
@@ -21,4 +21,3 @@ pred=logistic_model.predict(X_test)
 
 #ilkel başarı değeri
 print(f"İlkel başarı değeri : {accuracy_score(y_test,pred)}")
-
